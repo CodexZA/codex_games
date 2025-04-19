b74924396856af7a593997cec3aba1705ae19a3f
Cobus van Tonder <33369002+CodexZA@users.noreply.github.com>
2025-04-19 10:28:12 +0200

    Update fxmanifest.lua

--- codex_games/fxmanifest.lua
+++ codex_games/fxmanifest.lua
@@ -1,8 +1,16 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'Flip Coin & Roll Dice - QBox Compatible'
-author 'YourName'
+lua54 'yes'
+
+dependency 'qb-core'
+dependency 'ox_lib'
+
+shared_script '@ox_lib/init.lua'
+
+author 'CodexZA'
+description 'QBox Mini‑Games: Flip Coin, Roll Dice, Rock/Paper/Scissors'
 version '1.0.0'
 
 client_script 'client.lua'
+server_script 'server.lua'
